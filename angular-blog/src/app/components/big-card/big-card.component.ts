9b79660c4670ecd19ffad2d324abd10443131c8a
Edson_Victor <cruz.menezes@outlook.com>
2023-05-28 23:39:03 -0300

    mais um passo

--- angular-blog/src/app/components/big-card/big-card.component.ts
+++ angular-blog/src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+
+  @Input()
+  photoCover: string = "https://i.vimeocdn.com/video/1500221698-62d0dcf61c45a8061df3fdf6a46f957ed84915729d325e4868f632f559a37953-d_640"
+  cardTitle: string = "O que é Universo?"
+  cardDescription: string = "Na astronomia, o Universo corresponde ao conjunto de toda a matéria, energia, espaço e tempo existente. Ele reúne os astros: planetas, cometas, estrelas, galáxias, nebulosas, satélites, dentre outros. O universo é, portanto, mais que um local imenso, ele é tudo, e engloba tudo o que existe. Para muitos, infinito. Note que do latim, a palavra universum significa “todo inteiro” ou “tudo em um só”."
 
 }
